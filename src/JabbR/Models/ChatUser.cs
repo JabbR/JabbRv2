9d4d893c3b5c84c2e7f768b82c3d019b296da61f
Suhas Joshi <suhasbjoshi@hotmail.com>
2015-02-03 13:36:28 -0800

    Add Identity and EF

---
+++ src/JabbR/Models/ChatUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace JabbR.Models
+{
+    public class ChatUser : IdentityUser
+    {
+
+    }
+}
---
+++ src/JabbR/Models/JabbrContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+
+namespace JabbR.Models
+{
+    public class JabbrContext :IdentityDbContext<ChatUser>
+    {
+        private static bool _created = false;
+
+        public JabbrContext()
+        {
+            Database.AsMigrationsEnabled().ApplyMigrations();
+        }
+
+        protected override void OnConfiguring(DbContextOptions options)
+        {
+            options.UseSqlServer();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
