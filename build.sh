ac55ab9351f6bb46bb754d0d2c586655941e798e
alfhenrik <henrik.a.andersson@outlook.com>
2015-05-30 15:57:12 +1000

    Get a travis build running

--- build.sh
+++ build.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-rm -rf artifacts
-if ! type dnvm > /dev/null 2>&1; then
-    curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | sh && source ~/.dnx/dnvm/dnvm.sh
+
+rm -rf src/JabbR/wwwroot/lib
+
+if test `uname` = Darwin; then
+    cachedir=~/Library/Caches/KBuild
+else
+    if [ -z $XDG_DATA_HOME ]; then
+        cachedir=$HOME/.local/share
+    else
+        cachedir=$XDG_DATA_HOME;
+    fi
 fi
-export DNX_UNSTABLE_FEED=https://www.myget.org/F/aspnetrelease/api/v2
-dnvm install 1.0.0-beta5-11911
-dnu restore src/JabbR
+mkdir -p $cachedir
 
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+url=https://www.nuget.org/nuget.exe
 
-dnu publish src/JabbR --no-source --out artifacts/build/jabbr --runtime active 2>&1 | tee buildlog
-grep "Build succeeded" buildlog
+if test ! -f $cachedir/nuget.exe; then
+    wget -O $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url /dev/null
+fi
+
+if test ! -e .nuget; then
+    mkdir .nuget
+    cp $cachedir/nuget.exe .nuget/nuget.exe
+fi
+
+if test ! -d packages/KoreBuild; then
+    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+    mono .nuget/nuget.exe install Sake -version 0.2 -o packages -ExcludeVersion
+fi
+
+if ! type dnvm > /dev/null 2>&1; then
+    source packages/KoreBuild/build/dnvm.sh
+fi
+
+if ! type dnx > /dev/null 2>&1; then
+    dnvm upgrade
+fi
+
+mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
